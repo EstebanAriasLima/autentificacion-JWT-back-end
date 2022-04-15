60b7e8d91c3fc6239ee77fa3d2818b70d6447d3c
Esteban Arias <estebanariaslima@gmail.com>
2022-04-15 16:51:56 -0400

    se empujan los cambios de los endpoints

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import os
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
+from flask_jwt_extended import JWTManager,create_access_token,jwt_required, get_jwt_identity
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
@@ -15,10 +16,12 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = os.environ.get('FLASK_API_KEY')
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
+jwt = JWTManager(app)
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -39,7 +42,41 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
+@app.route('/sign-in', methods=['POST'])
+def create_user():
+    new_email=request.json.get("email",None)
+    registered_email=User.query.filter_by(email=new_email).first()
+    if registered_email is not None:
+        return jsonify({"msg":"El email ya está en uso"}),400
+    else:
+        body=request.json
+        user=User.create(
+            email=body['email'],
+            password=body['password'],
+        )
+        dictionary = user.serialize()
+        print(dictionary)
+        return jsonify(dictionary),201
+
+@app.route("/token",methods=['POST'])
+def create_token():
+    email=request.json.get("email",None)
+    password=request.json.get("password",None)
+    user=User.query.filter_by(email=email, password=password).first()
+    if user is None:
+        user=TravelerUser.query.filter_by(email=email, password=password).first()
+    if user is None:
+        return jsonify({"msg":"Error en el email o en la contraseña"}),401
+    access_token = create_access_token(identity=user.id)
+    return jsonify({ "token": access_token, "user_id": user.id})
+
+@app.route("/private", methods=["GET"])
+@jwt_required()
+def protected():
+    current_user_id = get_jwt_identity()
+    user = User.filter.get(current_user_id)
+    return jsonify({"id": user.id, "password": user.password }), 200
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
+    PORT = int(os.environ.get('PORT',5000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -6,8 +6,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
+    # is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    
     def __repr__(self):
         return '<User %r>' % self.username
 
@@ -17,3 +17,21 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+    @classmethod
+    def create(cls, **data):
+        #crear instancia
+        instance=cls(**data)
+        if (not isinstance(instance,cls)):
+            print('FALLA EL CONSTRUCTOR')
+            return None
+        #guardar en bdd
+        db.session.add(instance)
+        try:
+            db.session.commit()
+            return instance
+        except Exception as error:
+            print('FALLA BDD: ',error.args)
+            db.session.rollback()
+            return None
+            raise Exception(error.args, 500)
